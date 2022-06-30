f93fb8db11e9baf33807ad1be313efa9d1535892
unknown <you@example.com>
2022-06-30 15:16:00 +0000

    updated service

--- src/app/services/pokemon-data.service.ts
+++ src/app/services/pokemon-data.service.ts
@@ -16,28 +16,6 @@ export class PokemonDataService {
 
   constructor(private httpClient: HttpClient) { }
 
-  // getPokemonDataChamp1(name:string){
-  //   this.httpClient.get<any>(this.pokemonAPIChamp1+name.toLowerCase()).subscribe(data => {
-  //     console.log(data);
-  //     console.log(data.name);
-  //     console.log("HP is "+data.stats[0].base_stat);
-  //     console.log("Attack is "+data.stats[1].base_stat);
-  //     console.log("Defense is "+data.stats[2].base_stat);
-  //     console.log("Speed is "+data.stats[5].base_stat);
-  //   });
-  // }
-
-  // getPokemonDataChamp2(name:string){
-  //   this.httpClient.get<any>(this.pokemonAPIChamp2+name.toLowerCase()).subscribe(data => {
-  //     console.log(data);
-  //     console.log(data.name);
-  //     console.log("HP is "+data.stats[0].base_stat);
-  //     console.log("Attack is "+data.stats[1].base_stat);
-  //     console.log("Defense is "+data.stats[2].base_stat);
-  //     console.log("Speed is "+data.stats[5].base_stat);
-  //   });
-  // }
-
   getPokemonData(name:string):Observable<any>{
     return this.httpClient.get<any>(this.pokemonAPIChamp1+name.toLowerCase());
   }
