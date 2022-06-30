e1f0e517d7bf40104eb75cd842d4583b9aa089b3
unknown <you@example.com>
2022-06-30 15:36:46 +0000

    update branch

--- src/app/services/pokemon-data.service.ts
+++ src/app/services/pokemon-data.service.ts
@@ -7,21 +7,12 @@ import { Observable } from 'rxjs';
 })
 
 export class PokemonDataService {
-  constructor(private http:HttpClient) { }
 
-<<<<<<< HEAD
+  private pokemonAPIChamp1 = 'https://pokeapi.co/api/v2/pokemon/';
   constructor(private httpClient: HttpClient) { }
 
   getPokemonData(name:string):Observable<any>{
     return this.httpClient.get<any>(this.pokemonAPIChamp1+name.toLowerCase());
-=======
-  getPokemon(): Observable<any>{
-    var randomNumber:number = Math.floor(Math.random() * 50);
-    let apiURL:string = "https://pokeapi.co/api/v2/pokemon/"+randomNumber.toString();
-    let pokemonData = this.http.get(apiURL);
-    console.log(pokemonData);
-    return pokemonData;
->>>>>>> d36910e (merge)
   }
   
 }
