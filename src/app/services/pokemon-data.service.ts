fb7d62f8653f0ce6c352c0ce47ad30b2fbf412e8
unknown <you@example.com>
2022-06-30 15:10:33 +0000

    merge

--- src/app/services/pokemon-data.service.ts
+++ src/app/services/pokemon-data.service.ts
@@ -1,23 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError} from "rxjs";
-import { catchError, map, retry } from "rxjs/operators";
-import { Pokemon } from '../models/pokemon.model';
-// import { Pokemon } from '../components/pokemon/pokemon.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class PokemonDataService {
-  private pokemonAPIChamp1 = 'https://pokeapi.co/api/v2/pokemon/';
-  private pokemonAPIChamp2 = 'https://pokeapi.co/api/v2/pokemon/';
-  
+  constructor(private http:HttpClient) { }
 
+<<<<<<< HEAD
   constructor(private httpClient: HttpClient) { }
 
   getPokemonData(name:string):Observable<any>{
     return this.httpClient.get<any>(this.pokemonAPIChamp1+name.toLowerCase());
+=======
+  getPokemon(): Observable<any>{
+    var randomNumber:number = Math.floor(Math.random() * 50);
+    let apiURL:string = "https://pokeapi.co/api/v2/pokemon/"+randomNumber.toString();
+    let pokemonData = this.http.get(apiURL);
+    console.log(pokemonData);
+    return pokemonData;
+>>>>>>> d36910e (merge)
   }
   
 }
