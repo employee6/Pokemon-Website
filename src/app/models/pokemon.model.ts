4f7e6acf767693d7ce5d25b0425c0d08e501f6f9
Ghadi <ghadi.agha@turntabl.io>
2022-06-30 15:35:32 +0000

    updating css

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -8,11 +8,5 @@ export interface Pokemon {
         special_attack:number,
         special_defense:number,
         speed:number
-<<<<<<< HEAD
-    }];
-    sprites : [{
-        front_default: string,
-=======
->>>>>>> b27d2b128de891fb525b5816ec69ffd466cbb5f5
     }]
 }
